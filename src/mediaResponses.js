8016082285f52cee0bebeec464c015c3bff97554
KiernanBrown <kwb6721@g.rit.edu>
2018-01-29 03:07:18 -0500

    Finished Streaming Media Homework

---
+++ src/mediaResponses.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+
+// Function that takes in a filePath and contentType of the file and loads the file for streaming
+const loadFile = (request, response, filePath, contentType) => {
+  const file = path.resolve(__dirname, filePath); // Create our file object
+
+  fs.stat(file, (err, stats) => {
+    // If we have an Error No Entry, the file cannot be found, send the error back
+    if (err) {
+      if (err.code === 'ENOENT') {
+        response.writeHead(404);
+      }
+      return response.end(err);
+    }
+
+    // Get our range header
+    let { range } = request.headers;
+
+    // If there is not a range header, we start at the beginning of the file
+    if (!range) {
+      range = 'bytes=0-';
+    }
+
+    const positions = range.replace(/bytes=/, '').split('-');
+
+    let start = parseInt(positions[0], 10);
+
+    const total = stats.size;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+    if (start > end) {
+      start = end - 1;
+    }
+
+    const chunksize = (end - start) + 1;
+
+    response.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': contentType,
+    });
+
+    const stream = fs.createReadStream(file, { start, end });
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
+    return stream;
+  });
+};
+
+const getParty = (request, response) => {
+  loadFile(request, response, '../client/party.mp4', 'video/mp4');
+};
+
+const getBling = (request, response) => {
+  loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');
+};
+
+const getBird = (request, response) => {
+  loadFile(request, response, '../client/bird.mp4', 'video/mp4');
+};
+
+module.exports.getParty = getParty;
+module.exports.getBling = getBling;
+module.exports.getBird = getBird;
